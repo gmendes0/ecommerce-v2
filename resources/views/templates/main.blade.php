affdf26d8647efea64c20a4eab2faf1871b96972
gmendes0 <gmendes230@gmail.com>
2019-08-24 16:11:18 -0300

    Base template iniciada

---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"/>
+    @stack('cssarea')
+    <title>@yield('title')</title>
+  </head>
+
+  <body>
+    <p class="text-primary">aaaaaaaaaaaaaa</p>
+
+    <script src="{{ asset('assets/js/jquery.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+    @stack('jscripts')
+  </body>
+</html>
