b0d4d5ab99e143228d1a33d77a12339885d91936
gmendes0 <gmendes230@gmail.com>
2019-08-24 15:42:56 -0300

    migration fornecedores criada

---
+++ database/migrations/2019_08_24_183724_create_fornecedores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFornecedoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fornecedores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome', 100);
+            $table->string('email');
+            $table->string('cnpj', 14);
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fornecedores');
+    }
+}
